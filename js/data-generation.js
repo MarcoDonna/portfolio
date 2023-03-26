99bd2868e8be4a273f7fec8b6560ebaa30a1811e
MarcoDonna <marco.donna03@gmail.com>
2023-03-26 16:37:46 +0200

    generate cluster data

--- js/data-generation.js
+++ js/data-generation.js
@@ -18,3 +18,17 @@ function linearData(points, slope=1, offset=0, randomness=0){
     }
     return ret;
 }
+
+function clusterData(points, centerX, centerY, radius){
+    //https://jyopari.github.io/randomCirc.html
+    let ret = [];
+    for(let i = 0; i < points; i++){
+        const theta = Math.random() * 2 * Math.PI;
+        const r = Math.random() * radius;        
+        const x = centerX + Math.sqrt(r * radius) * Math.cos(theta);
+        const y = centerY + Math.sqrt(r * radius) * Math.sin(theta);
+
+        ret.push([x, y]);
+    }
+    return ret;
+}
